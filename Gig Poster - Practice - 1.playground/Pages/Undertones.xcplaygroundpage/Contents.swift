43d6e61ef47fb27d303a4d5b5d0d3a3d330c5a10
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2020-01-30 11:05:09 -0500

    got undertone to rotate around a point

--- Gig Poster - Practice - 1.playground/Pages/Undertones.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 1.playground/Pages/Undertones.xcplaygroundpage/Contents.swift
@@ -31,7 +31,63 @@ let deepRed = Color(hue: 7, saturation: 97, brightness: 72, alpha: 100)
 
 // Begin your solution here...
 
+////see where the origin is
+//canvas.drawAxes()
+//
+////move the origin 100 pixels right and 200 pixels up
+//canvas.translate(to: Point(x: 100, y: 200))
+//
+////see where the origin is after being translated
+//canvas.drawAxes()
+//
+////draw a square at the origin
+//canvas.drawRectangle(at: Point(x: 0, y: 0), width: 50, height: 50)
+//
+////make shapes have no fill
+//canvas.drawShapesWithFill = false
+//
+////move the origin again over 100, up 300
+//canvas.translate(to: Point(x: 100, y: 300))
+//
+////check to see where origin is
+//canvas.drawAxes()
+//
+////draw six squares rotated around the current origin
+//
+//for _ in 1...6 {
+//    //rotate the origin
+//    canvas.rotate(by: 60) //360/6 = 60
+//
+////    //draw axes
+////    canvas.drawAxes()
+//
+//    //draw square
+//    canvas.drawRectangle(at: Point(x: 0, y: 0), width: 50, height: 50)
+//}
 
+//start making poster here
+//create a light grey background
+canvas.fillColor = lightGrey
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+//move the origin 375 pixels up
+canvas.translate(to: Point(x: 0, y: 420))
+
+////draw axes to check where origin is
+//canvas.drawAxes()
+
+for _ in 1...19 {
+    //write 'undertones'
+    canvas.drawText(message: "undertones", at: Point(x: 20, y: -50), size: 67, kerning: 0)
+    
+    //rotate the origin
+    canvas.rotate(by: -4.7)
+    
+//    //draw axes to check the rotation
+//    canvas.drawAxes()
+    
+
+}
 /*:
  ## Use Source Control
  
